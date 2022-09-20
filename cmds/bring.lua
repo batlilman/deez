d3e3537f8b33214113f092ecef96e08955314a85
batlilman <113977891+batlilman@users.noreply.github.com>
2022-09-19 23:04:25 -1000

    Update bring.lua

--- cmds/bring.lua
+++ cmds/bring.lua
@@ -1,4 +1,4 @@
-a = function bringcmd(...)
+a = function(...)
     local data = {...}
     local players = game:service"Players"
     players.LocalPlayer.Character.HumanoidRootPart.CFrame=players[data[1]].Character.HumanoidRootPart.CFrame+Vector3.new(0,10,0)
