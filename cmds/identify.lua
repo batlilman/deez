fe5986f9895f34eaf264181bc8bf8563503dc115
batlilman <113977891+batlilman@users.noreply.github.com>
2022-09-20 21:18:42 -1000

    Update identify.lua

--- cmds/identify.lua
+++ cmds/identify.lua
@@ -1,7 +1,7 @@
 return function()
     local plr = game:service"Players".LocalPlayer
 
-    for _, v in pairs(Alts) do
+    for _, v in pairs(getgenv().Alts) do
         if tonumber(v) == plr.UserId then
             saymsg("I'm alt "..tostring(_))
         end
