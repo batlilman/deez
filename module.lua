32931395ce53597b0a2900a78e16ca91dc082af5
batlilman <113977891+batlilman@users.noreply.github.com>
2022-09-20 23:08:15 -1000

    Update module.lua

--- module.lua
+++ module.lua
@@ -11,3 +11,22 @@ end
 function saymsg(msg)
     game:service"ReplicatedStorage".DefaultChatSystemChatEvents.SayMessageRequest:FireServer(msg, 'All')
 end
+
+function CheckPlayer(userid)
+    for i,v in pairs(game:service"Players":GetChildren()) do
+        if v.UserId == userid then
+            return true
+        end
+    end
+    return false
+end
+
+function ReSort(table_)
+    local k = {}
+    for i,v in pairs(table_) do
+        if CheckPlayer(v) == true then
+            table.insert(k,v)
+        end
+    end
+    return k
+end
