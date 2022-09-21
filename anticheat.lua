9bcf5a360cc3bf70a0f6dff378244f21598f1ac3
batlilman <113977891+batlilman@users.noreply.github.com>
2022-09-20 23:05:07 -1000

    Create anticheat.lua

---
+++ anticheat.lua
@@ -0,0 +1,18 @@
+local remotes = {
+    "CHECKER_1",
+    "CHECKER_2",
+    "TeleportDetect",
+    "OneMoreTime",
+    "BreathingHAMON",
+    "VirusCough",
+}
+
+local __namecall
+__namecall = hookmetamethod(game, "__namecall", function(...)
+    local args = {...}
+    local method = getnamecallmethod()
+    if (method == "FireServer" and args[1] == game:service"ReplicatedStorage".MainEvent and table.find(remotes, args[2])) then
+        return
+    end
+    return __namecall(...)
+end)
