79cc64561fb29b4ceb72f9fcb4a85ca73f9a61c0
batlilman <113977891+batlilman@users.noreply.github.com>
2022-09-23 17:52:45 -1000

    Create advert.lua

---
+++ cmds/advert.lua
@@ -0,0 +1,24 @@
+local Adverting = false
+
+return function(...)
+    local data = ProccessArgs(...)
+    if data[2]:lower() == "set" then
+        local NewMsg = ""
+        for i=3,#data then
+            if i == #data then
+                NewMsg = NewMsg..""..tostring(data[i])
+            else
+                NewMsg = NewMsg..""..tostring(data[i]).." "
+            end
+        end
+        Settings['admsg'] = NewMsg
+    elseif data[2]:lower() == "on" then
+        Adverting = true
+        while Adverting == true do
+            wait(1)
+            saymsg(Settings['admsg'])
+        end
+    elseif data[2]:lower() == "off" then
+        Adverting = false
+    end
+end
